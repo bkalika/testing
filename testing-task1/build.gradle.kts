e75893b3b111717fdcda0d6f7cca46f830e91f0d
bkalika <bogdan.kalika@gmail.com>
2024-10-19 18:30:33 +0300

    Commit aster rebase from remote

--- testing-task1/build.gradle.kts
+++ testing-task1/build.gradle.kts
@@ -3,6 +3,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.2")
     testImplementation("org.hamcrest:hamcrest:3.0")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+    testImplementation("org.awaitility:awaitility:4.2.2")
 }
 
 tasks.test {

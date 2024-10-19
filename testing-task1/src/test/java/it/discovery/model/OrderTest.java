af9864ed7aea75a5667bbfd7661af2e95b53ec7f
Sergiy <sergey.morenets@gmail.com>
2024-10-19 16:04:41 +0300

    Task 5

--- testing-task1/src/test/java/it/discovery/model/OrderTest.java
+++ testing-task1/src/test/java/it/discovery/model/OrderTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static it.discovery.util.TestModelUtils.createBook;
+import static jdk.dynalink.linker.support.Guards.isNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -39,7 +42,13 @@ class OrderTest {
     }
 
     @Test
-    void createOrder() {
+    void createOrder_validArgumentValues_returnsOrder() {
+        var book = createBook();
+        var order1 = Order.createOrder(book, 10);
+        assertThat(order1, notNullValue());
+        assertThat(order1.getItems(), hasSize(1));
+        assertThat(order1.getItems().getFirst().getBook(), equalTo(book));
+        assertThat(order1.getItems().getFirst().getAmount(), equalTo(10));
     }
 
     @Test

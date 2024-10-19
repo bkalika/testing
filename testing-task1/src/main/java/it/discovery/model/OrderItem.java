b2d91dc37fde12c8713d22b9342f188ae3413871
bkalika <bogdan.kalika@gmail.com>
2024-10-19 15:53:31 +0300

    Commit aster rebase from remote

--- testing-task1/src/main/java/it/discovery/model/OrderItem.java
+++ testing-task1/src/main/java/it/discovery/model/OrderItem.java
@@ -1,12 +1,12 @@
 package it.discovery.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class OrderItem {
 	
 	private Book book;
